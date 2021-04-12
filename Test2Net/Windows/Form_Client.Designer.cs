ae8f5f3a8fd03e72471f790862afaba5416d4ae6
Alexander Ponomarev <dom2564@icloud.com>
2021-04-12 11:19:43 +0500

    Убрали возможность менять размер окон. Изменили центрирование окон. Уменьшили окно диспетчера

--- Test2Net/Windows/Form_Client.Designer.cs
+++ Test2Net/Windows/Form_Client.Designer.cs
@@ -51,9 +51,9 @@
             this.panel1.Controls.Add(this.lblName);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(334, 163);
+            this.panel1.Size = new System.Drawing.Size(445, 201);
             this.panel1.TabIndex = 0;
             // 
             // cmbbxName
@@ -64,9 +64,10 @@
             "Станок Василий",
             "Валли",
             "Мэтр"});
-            this.cmbbxName.Location = new System.Drawing.Point(94, 12);
+            this.cmbbxName.Location = new System.Drawing.Point(125, 15);
+            this.cmbbxName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbbxName.Name = "cmbbxName";
-            this.cmbbxName.Size = new System.Drawing.Size(228, 21);
+            this.cmbbxName.Size = new System.Drawing.Size(303, 24);
             this.cmbbxName.TabIndex = 8;
             // 
             // lblError
@@ -75,28 +76,29 @@
             this.lblError.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lblError.ForeColor = System.Drawing.Color.Green;
-            this.lblError.Location = new System.Drawing.Point(0, 136);
+            this.lblError.Location = new System.Drawing.Point(0, 168);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(334, 27);
+            this.lblError.Size = new System.Drawing.Size(445, 33);
             this.lblError.TabIndex = 7;
             this.lblError.Text = "Хорошо!";
             this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // pbar
             // 
-            this.pbar.Location = new System.Drawing.Point(11, 95);
-            this.pbar.Margin = new System.Windows.Forms.Padding(2);
+            this.pbar.Location = new System.Drawing.Point(15, 117);
+            this.pbar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbar.Name = "pbar";
-            this.pbar.Size = new System.Drawing.Size(312, 32);
+            this.pbar.Size = new System.Drawing.Size(416, 39);
             this.pbar.TabIndex = 4;
             // 
             // btnConnect
             // 
             this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnConnect.Location = new System.Drawing.Point(222, 41);
-            this.btnConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnect.Location = new System.Drawing.Point(296, 50);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(101, 36);
+            this.btnConnect.Size = new System.Drawing.Size(135, 44);
             this.btnConnect.TabIndex = 3;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -106,10 +108,9 @@
             // 
             this.lblStatusUI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lblStatusUI.ForeColor = System.Drawing.Color.Red;
-            this.lblStatusUI.Location = new System.Drawing.Point(89, 41);
-            this.lblStatusUI.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatusUI.Location = new System.Drawing.Point(119, 50);
             this.lblStatusUI.Name = "lblStatusUI";
-            this.lblStatusUI.Size = new System.Drawing.Size(135, 30);
+            this.lblStatusUI.Size = new System.Drawing.Size(180, 37);
             this.lblStatusUI.TabIndex = 2;
             this.lblStatusUI.Text = "Not Connection";
             this.lblStatusUI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -117,10 +118,9 @@
             // lblStatus
             // 
             this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblStatus.Location = new System.Drawing.Point(12, 41);
-            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatus.Location = new System.Drawing.Point(16, 50);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(62, 30);
+            this.lblStatus.Size = new System.Drawing.Size(83, 37);
             this.lblStatus.TabIndex = 1;
             this.lblStatus.Text = "Status:";
             this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,25 +128,26 @@
             // lblName
             // 
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblName.Location = new System.Drawing.Point(10, 9);
-            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Location = new System.Drawing.Point(13, 11);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(70, 31);
+            this.lblName.Size = new System.Drawing.Size(93, 38);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Name:";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form_Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 163);
+            this.ClientSize = new System.Drawing.Size(445, 201);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Location = new System.Drawing.Point(500, 250);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(463, 244);
             this.Name = "Form_Client";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "Form Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиент";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Client_FormClosing);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
